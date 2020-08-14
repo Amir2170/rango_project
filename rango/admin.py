64655c016034a84cc31afaefb181e8b5ed773cca
Amir2170 <amirrezaamp2170@gmail.com>
2020-08-14 09:20:28 -0700

    completed chapter 6 of tango with django

--- rango/admin.py
+++ rango/admin.py
@@ -4,5 +4,24 @@ from .models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
+class PageInline(admin.TabularInline):
+	model = Page
+	extra = 10
+
+class CategoryAdmin(admin.ModelAdmin):
+	fieldsets=[
+		('Name of category', {'fields': ['name'], 'classes':
+			['collapse']}),
+		('Number of likes',  {'fields': ['likes'],'classes': 
+			['collapse']}),
+		('Number of views',  {'fields': ['views'], 'classes':
+			['collapse']}),
+		]
+	inlines = [PageInline]
+	list_display = ('name', 'views', 'likes')
+	list_filter = ['name']
+	search_fields = ['name']
+	
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
